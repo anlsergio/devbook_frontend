82eb608f44bc11e1652f4fce1696f34e4a7b8fc1
Anderson L. Sergio <anderson.als04@gmail.com>
2021-04-06 17:46:59 -0300

    Retrieve posts data for the home page (feed)

--- src/controllers/pages.go
+++ src/controllers/pages.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/models"
 	"webapp/src/requests"
 	"webapp/src/responses"
 	"webapp/src/utils"
@@ -34,5 +36,11 @@ func RenderHomePage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utils.RenderTemplate(w, "home.html", nil)
+	var posts []models.Post
+	if err := json.NewDecoder(response.Body).Decode(&posts); err != nil {
+		responses.JSON(w, http.StatusUnprocessableEntity, responses.APIError{Error: err.Error()})
+		return
+	}
+
+	utils.RenderTemplate(w, "home.html", posts)
 }
